f124246aff79bcf9b99fe47ec1e825d23b4a3468
r0man-x <snegovata@gmail.com>
2020-10-03 21:10:40 +0300

    refactor: added funtions
    refactor: added funtions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+    if (!array || array.length === 0) return 0;
+    let currentMin = array[0];
+    array.forEach((value) => {
+      if (value < currentMin) currentMin = value;
+    })
+    return currentMin;
+  }
+  
+  exports.max = function max (array) {
+    if (!array || array.length === 0) return 0;
+    let currentMax = array[0];
+    array.forEach((value) => {
+      if (value > currentMax) currentMax = value;
+    })
+    return currentMax;
+  }
+  
+  exports.avg = function avg (array) {
+    if (!array || array.length === 0) return 0;
+    return array.reduce((total, current) => total += current) / array.length;
+  }
